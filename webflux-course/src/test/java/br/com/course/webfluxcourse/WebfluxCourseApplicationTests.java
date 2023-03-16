c39649ed274ecb7179c5fa33cfdd7f4f954c99b8
Vitor <vitordacruz@gmail.com>
2023-03-15 21:14:25 -0300

    Criado o Projeto Inicial

---
+++ webflux-course/src/test/java/br/com/course/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
