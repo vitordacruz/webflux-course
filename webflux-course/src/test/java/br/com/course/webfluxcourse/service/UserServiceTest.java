36969774f948c863ecffe6f9548dbd5890f4e71f
Vitor <vitordacruz@gmail.com>
2023-03-23 20:46:10 -0300

    Adicionado teste unitário no método salvar do UserService

---
+++ webflux-course/src/test/java/br/com/course/webfluxcourse/service/UserServiceTest.java
@@ -0,0 +1,42 @@
+package br.com.course.webfluxcourse.service;
+
+import br.com.course.webfluxcourse.entity.User;
+import br.com.course.webfluxcourse.mapper.UserMapper;
+import br.com.course.webfluxcourse.model.request.UserRequest;
+import br.com.course.webfluxcourse.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService service;
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("Vitor", "vitor@mail.com", "123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result =  service.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user instanceof User)
+                .expectComplete()
+                .verify();
+    }
+}
